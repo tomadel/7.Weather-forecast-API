a1bbe626154cca18fbfba276a28c6e174b3c6bef
Guney <aliguneydogan@hotmail.com>
2025-02-15 19:57:30 +0300

    Added the images for the sky. Added try and catch block to prevent the input errors for the city names

--- backend.py
+++ backend.py
@@ -2,18 +2,14 @@ import requests
 
 API_KEY = "d610392fc9529e3ac3fbc794fb7016f0"
 
-def get_data(place, forecast_days=None, weather_type=None):
+def get_data(place, forecast_days=None):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
     filtered_data = data["list"]
     nr_values = 8 * forecast_days
     filtered_data = filtered_data[:nr_values]
-    if weather_type == "Temperature":
-        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
-    if weather_type == "Sky":
-        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
-    return data
+    return filtered_data
 
 if __name__ == "__main__":
-    get_data(place="Tokyo", forecast_days=3, weather_type="Temperature")
+    get_data(place="Tokyo", forecast_days=3)
